65b0fa8d8d8f735a88b95565544221b6d7be95cb
mustafa-mehboob <mustafamehboob996@gmail.com>
2021-02-07 19:42:45 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React, {useState} from "react";
 import './App.css';
 import Parent from "./Parent";
 import CounterContext from "./CounterContext";
+import { useState } from 'react';
 
 function App() {
+
+  let [count, SetCount] = useState(75) 
   return (
-    <CounterContext.Provider value={19}>
+    <CounterContext.Provider value={count}>
       <div>
         <Parent name="Mustafa" />
       </div>
